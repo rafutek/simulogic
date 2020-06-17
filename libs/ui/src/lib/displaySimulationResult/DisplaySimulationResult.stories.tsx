9065240ba915f863b9111182ac01192f2785e7e2
Raphaël Roy <rafutek@protonmail.com>
2020-06-17 14:22:26 -0400

    display simulation result story

---
+++ libs/ui/src/lib/displaySimulationResult/DisplaySimulationResult.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { DisplaySimulationResult } from './DisplaySimulationResult';
+
+export default {
+    title: 'Display simulation result',
+    component: DisplaySimulationResult,
+};
+
+export const Default = () => <DisplaySimulationResult />
--- libs/ui/src/lib/displaySimulationResult/DisplaySimulationResult.tsx
+++ libs/ui/src/lib/displaySimulationResult/DisplaySimulationResult.tsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { Entity } from '../core'
+import { Entity, Simulation } from '../core'
 import { SelectEntity } from '../selectEntity/SelectEntity';
 import axios from 'axios';
+import { SimulationDisplay } from '../simulationDisplay/SimulationDisplay';
 
-export const SelectAndGetCircuitSimulation = () => {
+export const DisplaySimulationResult = () => {
 
     const [selectedCircuit, setSelectedCircuit] = useState<Entity>();
     const [selectedSimulation, setSelectedSImulation] = useState<Entity>();
+    const [simulationResult, setSimulationResult] = useState<Simulation>();
 
     useEffect(() => {
         if( selectedCircuit && selectedSimulation){
             axios.get(`/simulations/${selectedCircuit.id}/${selectedSimulation.id}`)
             .then((response) => {
-                console.log(response.data);
+                setSimulationResult(response.data);
             })
             .catch((error) => {
                 console.log(error);
@@ -24,6 +26,7 @@ export const SelectAndGetCircuitSimulation = () => {
         <div>
             <SelectEntity entity="circuit" setSelectedEntity={setSelectedCircuit} />
             <SelectEntity entity="simulation" setSelectedEntity={setSelectedSImulation} />
+            {simulationResult ? <SimulationDisplay simulation={simulationResult} /> : null}
         </div>
     )
 }
