7d3ecfff42b989002502f60734b3b36c66c388d4
Raphaël Roy <rafutek@protonmail.com>
2020-07-03 17:19:36 -0400

    test init interval wavedrom

--- apps/server/src/app/extractors/extractors.service.spec.ts
+++ apps/server/src/app/extractors/extractors.service.spec.ts
@@ -37,38 +37,57 @@ describe('SimulationExtractor', () => {
     })
   });
 
-  describe('fillIntervalWaveDrom', () => {
-
-    it("should return the right interval wavedrom", () => {
-      // Given a wavedrom with two signals
-      // and an initialized interval wavedrom
-      const wavedrom: WaveDrom = {
-        signal: [],
-        foot: {
-          tick: "x 0 100 200 350 x "
-        }
+  describe('Interval WaveDrom Tests', () => {
+    const wavedrom: WaveDrom = {
+      signal: [],
+      foot: {
+        tick: "x 0 100 200 350 x "
       }
-      const s1: Signal = {
-        name: "s1",
-        wave: "x0.1.x"
-      };
-      const s2: Signal = {
-        name: "s2",
-        wave: "x.1.0x"
-      };
-      wavedrom.signal.push(s1, s2);
-      let interval_wavedrom = extractor.initIntervalWaveDrom(wavedrom);
+    }
+    const s1: Signal = {
+      name: "s1",
+      wave: "x0.1.x"
+    };
+    const s2: Signal = {
+      name: "s2",
+      wave: "x.1.0x"
+    };
+    wavedrom.signal.push(s1, s2);
 
-      // When filling the interval wavedrom
-      const from = 90, to = 250;
-      interval_wavedrom = extractor.fillIntervalWaveDrom(interval_wavedrom, wavedrom, from, to);
+    describe('initIntervalWaveDrom', () => {
 
-      // Then the interval wavedrom must contain these values
-      const tick = "100 200 ";
-      const interval_wave_s1 = ".1", interval_wave_s2 = "1.";
-      expect(interval_wavedrom.foot.tick).toEqual(tick);
-      expect(interval_wavedrom.signal[0].wave).toEqual(interval_wave_s1);
-      expect(interval_wavedrom.signal[1].wave).toEqual(interval_wave_s2);
-    })
+      it("should initialize the interval wavedrom", () => {
+        // Given a wavedrom with two signals
+        // When initilizing the interval wavedrom
+        const interval_wavedrom = extractor.initIntervalWaveDrom(wavedrom);
+
+        // Then the interval wavedrom must respect these conditions
+        expect(interval_wavedrom.foot.tick).toEqual("");
+        expect(interval_wavedrom.signal[0].name == s1.name &&
+          interval_wavedrom.signal[0].wave == "").toBeTruthy();
+        expect(interval_wavedrom.signal[1].name == s2.name &&
+          interval_wavedrom.signal[1].wave == "").toBeTruthy();
+      })
+    });
+
+    describe('fillIntervalWaveDrom', () => {
+
+      it("should return the right interval wavedrom", () => {
+        // Given a wavedrom with two signals
+        // and an initialized interval wavedrom
+        let interval_wavedrom = extractor.initIntervalWaveDrom(wavedrom);
+
+        // When filling the interval wavedrom
+        const from = 90, to = 250;
+        interval_wavedrom = extractor.fillIntervalWaveDrom(interval_wavedrom, wavedrom, from, to);
+
+        // Then the interval wavedrom must contain these values
+        const tick = "100 200 ";
+        const interval_wave_s1 = ".1", interval_wave_s2 = "1.";
+        expect(interval_wavedrom.foot.tick).toEqual(tick);
+        expect(interval_wavedrom.signal[0].wave).toEqual(interval_wave_s1);
+        expect(interval_wavedrom.signal[1].wave).toEqual(interval_wave_s2);
+      })
+    });
   });
 });
