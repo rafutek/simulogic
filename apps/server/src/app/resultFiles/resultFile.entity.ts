b81099ad2c07ebe3345250ccfe73ebed164dd73c
Raphaël Roy <rafutek@protonmail.com>
2020-10-22 11:06:24 +0200

    fix resultFile cascade relations

--- apps/server/src/app/resultFiles/resultFile.entity.ts
+++ apps/server/src/app/resultFiles/resultFile.entity.ts
@@ -10,11 +10,11 @@ export class ResultFile {
   @Column()
   path: string;
 
-  @OneToOne(type => CircuitFile, { cascade: true })
+  @OneToOne(type => CircuitFile, { cascade: true, onDelete: "CASCADE", primary: true })
   @JoinColumn()
   circuit_file: CircuitFile;
 
-  @OneToOne(type => SimulationFile, { cascade: true })
+  @OneToOne(type => SimulationFile, { cascade: true, onDelete: "CASCADE", primary: true })
   @JoinColumn()
   simulation_file: SimulationFile;
 }
